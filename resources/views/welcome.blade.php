a7d02cf5bfad603048a8d183dede685a20e0bd1a
Benjamin Dehli <superelg@gmail.com>
2015-02-24 00:02:06 +0100

    Legg til pur regnskap velkomstskjerm

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,22 @@
 				width: 100%;
 				height: 100%;
 				color: #B0BEC5;
+                color: #eee;
 				display: table;
 				font-weight: 100;
 				font-family: 'Lato';
-			}
+                background: #23252a; /* Old browsers */
+                /* IE9 SVG, needs conditional override of 'filter' to 'none' */
+                background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPHJhZGlhbEdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgY3g9IjUwJSIgY3k9IjUwJSIgcj0iNzUlIj4KICAgIDxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiMyMzI1MmEiIHN0b3Atb3BhY2l0eT0iMSIvPgogICAgPHN0b3Agb2Zmc2V0PSIxMDAlIiBzdG9wLWNvbG9yPSIjMTExMjE0IiBzdG9wLW9wYWNpdHk9IjEiLz4KICA8L3JhZGlhbEdyYWRpZW50PgogIDxyZWN0IHg9Ii01MCIgeT0iLTUwIiB3aWR0aD0iMTAxIiBoZWlnaHQ9IjEwMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
+                background: -moz-radial-gradient(center, ellipse cover,  #23252a 0%, #111214 100%); /* FF3.6+ */
+                background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%,#23252a), color-stop(100%,#111214)); /* Chrome,Safari4+ */
+                background: -webkit-radial-gradient(center, ellipse cover,  #23252a 0%,#111214 100%); /* Chrome10+,Safari5.1+ */
+                background: -o-radial-gradient(center, ellipse cover,  #23252a 0%,#111214 100%); /* Opera 12+ */
+                background: -ms-radial-gradient(center, ellipse cover,  #23252a 0%,#111214 100%); /* IE10+ */
+                background: radial-gradient(ellipse at center,  #23252a 0%,#111214 100%); /* W3C */
+                filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#23252a', endColorstr='#111214',GradientType=1 ); /* IE6-8 fallback on horizontal gradient */
+
+            }
 
 			.container {
 				text-align: center;
@@ -33,13 +45,32 @@
 			.quote {
 				font-size: 24px;
 			}
+            
+            a.button {
+                color: #eee;
+                text-decoration: none;
+                padding: 7px 10px;
+                border: 1px solid #eee;
+                margin-top: 20px;
+                display: inline-block;
+                transition: all .1s;
+                border-radius: 2px;
+                text-transform: uppercase;
+            }
+
+            a.button:hover{
+                background-color: #eee;
+                color: #23252a;
+            }
 		</style>
 	</head>
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
+				<div class="title">pur:Regnskap</div>
+				<!--<div class="quote">{{ Inspiring::quote() }}</div> -->
+                <div class="quote">Tren på å føre regnskap - og bare det!</div>
+                <a href="home" class="button">Kom igang</a>
 			</div>
 		</div>
 	</body>
