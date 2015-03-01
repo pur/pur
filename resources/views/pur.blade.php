03d90474f3237b1bfb5e189d5837bef949e133dc
Benjamin Dehli <superelg@gmail.com>
2015-03-01 20:20:15 +0100

    Legg til FontAwesome

--- resources/views/pur.blade.php
+++ resources/views/pur.blade.php
@@ -4,8 +4,45 @@
     <meta charset="utf-8">
     <link href="/css/app.css" rel="stylesheet">
     <link href="/css/pur.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
 </head>
 <body>
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">pur:Regnskap</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li><a href="/">Hjem</a></li>
+            </ul>
+
+            <ul class="nav navbar-nav navbar-right">
+                @if (Auth::guest())
+                    <li><a href="/auth/login">Logg inn</a></li>
+                    <li><a href="/auth/register">Registrer</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="/auth/logout">Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
 
 @yield('content')
 
