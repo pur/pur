b9fbcd6bea79bbd94d8b5874e67cb1fc71308b77
Benjamin Dehli <superelg@gmail.com>
2015-03-10 22:21:56 +0100

    Legg til dummymoduler med testbilde på home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,72 @@
 @extends('pur')
 
 @section('content')
-<div class="container">
-	<div class="row">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-default">
-				<div class="panel-heading">Home</div>
+    <div class="alert alert-info alert-dismissible fade in" role="alert">
+        You are logged in!
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>
+    </div>
 
-				<div class="panel-body">
-					You are logged in!
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+    <div class="row">
+        <div class="col-xs-6 col-sm-4 col-md-3">
+            <div class="thumbnail">
+                <img src="{{ env('APPWEBPATH') }}/images/module-thumbs/test-thumb.jpg" alt="...">
+
+                <div class="caption">
+                    <h3>pur:Regnskap</h3>
+
+                    <p>Tren på å føre regnskap - og bare det!</p>
+
+                    <p><a href="#" class="btn btn-primary" role="button">Velg</a>
+                        <a href="#" class="btn btn-default" role="button">Info</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4 col-md-3">
+            <div class="thumbnail">
+                <img src="{{ env('APPWEBPATH') }}/images/module-thumbs/test-thumb.jpg" alt="...">
+
+                <div class="caption">
+                    <h3>pur:SQL</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+                    <p><a href="#" class="btn btn-primary" role="button">Velg</a>
+                        <a href="#" class="btn btn-default" role="button">Info</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4 col-md-3">
+            <div class="thumbnail">
+                <img src="{{ env('APPWEBPATH') }}/images/module-thumbs/test-thumb.jpg" alt="...">
+
+                <div class="caption">
+                    <h3>pur:Java</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+                    <p><a href="#" class="btn btn-primary" role="button">Velg</a>
+                        <a href="#" class="btn btn-default" role="button">Info</a></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-6 col-sm-4 col-md-3">
+            <div class="thumbnail">
+                <img src="{{ env('APPWEBPATH') }}/images/module-thumbs/test-thumb.jpg" alt="...">
+
+                <div class="caption">
+                    <h3>pur:Statistikk</h3>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+
+                    <p><a href="#" class="btn btn-primary" role="button">Velg</a>
+                        <a href="#" class="btn btn-default" role="button">Info</a></p>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
 @endsection
