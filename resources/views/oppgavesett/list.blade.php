67157be17b5464a6c77130ee7678eebd4dcaafe0
Benjamin Dehli <superelg@gmail.com>
2015-03-11 00:08:03 +0100

    Legg til kolonner i list oppgavesett

--- resources/views/oppgavesett/list.blade.php
+++ resources/views/oppgavesett/list.blade.php
@@ -3,10 +3,31 @@
 
     <h1>Alle oppgavesett</h1>
     <div class="list-group panel panel-primary" id="accordion" role="tablist" aria-multiselectable="true">
-        <div class="panel-heading">
+        <div class="panel-heading hidden-xs">
             <div class=" row">
-                <div class="col-sm-8">
+                <div class="col-sm-1">
+                    {!! Form::checkbox('', '') !!}
+                </div>
+                <div class="col-sm-2">
+                    <b>Opprettet:</b>
+                </div>
+                <div class="col-sm-2">
                     <b>Beskrivelse:</b>
+                </div>
+                <div class="col-sm-1">
+                    <b>Fullførte:</b>
+                </div>
+                <div class="col-sm-1">
+                    <b>Status:</b>
+                </div>
+                <div class="col-sm-2">
+                    <b>Åpen fra:</b>
+                </div>
+                <div class="col-sm-2">
+                    <b>Åpen til:</b>
+                </div>
+                <div class="col-sm-1">
+                    <b>Stats:</b>
                 </div>
             </div>
         </div>
@@ -14,11 +35,30 @@
         @foreach($alleoppgavesett as $oppgavesett)
             <div class="list-group-item">
                 <div class="row">
-                    <div class="col-sm-10">
-                        {!! link_to_route('oppgavesett.show', $oppgavesett->beskrivelse, [$oppgavesett->id]) !!}
+
+                    <div class="col-sm-1">
+                        {!! Form::checkbox('', '') !!}
                     </div>
                     <div class="col-sm-2">
-                        <a href="{!! $oppgavesett->id !!}/">Vis</a>
+                        <span class="visible-xs-inline">Opprettet: </span>{!! $oppgavesett->tid_opprettet !!}
+                    </div>
+                    <div class="col-sm-2">
+                        <span class="visible-xs-inline">Beskrivelse: </span>{!! link_to_route('oppgavesett.show', $oppgavesett->beskrivelse, [$oppgavesett->id]) !!}
+                    </div>
+                    <div class="col-sm-1">
+                        <span class="visible-xs-inline">Fullførte: </span>56/100
+                    </div>
+                    <div class="col-sm-1">
+                        <span class="visible-xs-inline">Status: </span>Åpen
+                    </div>
+                    <div class="col-sm-2">
+                        <span class="visible-xs-inline">Åpen fra: </span>{!! $oppgavesett->tid_aapent !!}
+                    </div>
+                    <div class="col-sm-2">
+                        <span class="visible-xs-inline">Åpen til: </span>{!! $oppgavesett->tid_lukket !!}
+                    </div>
+                    <div class="col-sm-1">
+                        <a href="{!! $oppgavesett->id !!}/"><span class="fa fa-bar-chart"></span> </a>
                     </div>
                 </div>
             </div>
