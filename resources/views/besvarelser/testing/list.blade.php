7eaefce32ac476f95bd589389272ddd9fa842624
Benjamin Dehli <superelg@gmail.com>
2015-03-31 15:27:06 +0200

    Oppdater besvarelser GUI

--- resources/views/besvarelser/testing/list.blade.php
+++ resources/views/besvarelser/testing/list.blade.php
@@ -1,13 +1,73 @@
 @extends('pur')
 @section('content')
+    @include('besvarelser.testing._listSubmenu')
+    <div class="container">
+        <h1>Mine oppgavesett</h1>
 
+        <div class="list-group panel panel-primary" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel-heading hidden-xs">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <b>Oppgave:</b>
+                    </div>
+                    <div class="col-sm-2">
+                        <b>Fremdrift:</b>
+                    </div>
+                    <div class="col-sm-2">
+                        <b>Status:</b>
+                    </div>
+                    <div class="col-sm-2">
+                        <b>Frist:</b>
+                    </div>
+                    <div class="col-sm-2">
+                        <b>Tid levert:</b>
+                    </div>
+                </div>
+            </div>
 
-    <h1>Alle besvarelser</h1>
-    <ul>
-        @foreach($besvarelser as $besvarelse)
-            <li>{{ $besvarelse->skaper->fulltnavn() . ' sin besvarelse på ' . $besvarelse->oppgavesett->beskrivelse }}
-                {!! link_to_route('besvarelser.show', 'Se besvarelse', [$besvarelse->id]) !!}</li>
-        @endforeach
-    </ul>
+            @foreach($besvarelser as $besvarelse)
+                <div class="list-group-item">
+                    <div class="row">
+                        <div class="col-sm-2">
+                            <span class="visible-xs-inline">Beskrivelse: </span>{{ $besvarelse->oppgavesett->beskrivelse}}
+                        </div>
 
+                        <div class="col-sm-2">
+                            <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                    60%
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-2">
+                            <span class="visible-xs-inline">Status: </span>{{ $besvarelse->oppgavesett->oppgaver()->count() }}
+                        </div>
+                        <div class="col-sm-2">
+                            <span class="visible-xs-inline">Frist: </span>{{ $besvarelse->oppgavesett->tidLukket() }}
+                        </div>
+                        <div class="col-sm-2">
+                            <span class="visible-xs-inline">Tid levert: </span>{{ $besvarelse->tidLevert() }}
+                        </div>
+                        <div class="col-sm-2">
+                            <div class="btn-group pull-right">
+                                @if($besvarelse->tidLevert() == null)
+                                    <a href="" class="btn btn-default" data-toggle="tooltip" data-placement="top" data-container="body" title="Fortsett oppgave">
+                                        <span class="fa fa-play-circle"></span>
+                                    </a>
+                                @endif
+                                <a href="" class="btn btn-default" data-toggle="tooltip" data-placement="top" data-container="body" title="Vis besvarelse">
+                                    <span class="fa fa-eye"></span>
+                                </a>
+                                <a href="" class="btn btn-default" data-toggle="tooltip" data-placement="top" data-container="body" title="Rediger besvarelse">
+                                    <span class="fa fa-edit"></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            @endforeach
+
+        </div>
+    </div>
 @endsection
--- resources/views/pur.blade.php
+++ resources/views/pur.blade.php
@@ -39,8 +39,7 @@
                     <a href="/" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Mine
                         oppgaver <span class="caret"></span></a>
                     <ul class="dropdown-menu" role="menu">
-                        <li>{!! link_to_route('oppgavesett.show', 'Oppgavesett', null) !!}</li>
-                        <li>{!! link_to_route('bilagsmalsekvenser.show', 'Bilagsmalsekvenser', null) !!}</li>
+                        <li>{!! link_to_route('besvarelser.show', 'Mine oppgavesett', null) !!}</li>
                     </ul>
                 </li>
 
