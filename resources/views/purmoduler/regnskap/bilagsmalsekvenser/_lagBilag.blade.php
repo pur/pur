1c7668a8733cd332aafcce8179f493181bdf52f2
Benjamin Dehli <superelg@gmail.com>
2015-04-13 11:21:27 +0200

    Oppdater GUI

--- resources/views/purmoduler/regnskap/bilagsmalsekvenser/_lagBilag.blade.php
+++ resources/views/purmoduler/regnskap/bilagsmalsekvenser/_lagBilag.blade.php
@@ -12,28 +12,36 @@
             </div>
         </div>
 
-
-        <p class="lead">Felter som skal vises:</p>
-
         <div class="row">
-            <div class="form-group col-sm-4">
-                {!! Form::checkbox('false', 'motpartEksempel' . $bilagsmal->id) !!} Motpart:
-                <span class="motpartEksempel">{{$bilagsmalsekvens->motpart}}</span>
-            </div>
+            <div class="col-md-12">
+                <p class="lead">Felter som skal vises:</p>
 
-            @foreach($bilagsmalsekvens->variabler as $variabel)
-                <div class="form-group col-sm-4">
-                    {!! Form::checkbox('false', $variabel->tegn_i_formel . 'Eksempel' . $bilagsmal->id) !!}
-                    {{$variabel->tegn_i_formel}}:
-                    <span class="{{$variabel->tegn_i_formel}}NavnEksempel">{{$variabel->navn}}:</span>
-                    <span class="{{$variabel->tegn_i_formel}}Eksempel">{{($variabel->verdi_min +$variabel->verdi_min)/2}}</span>
+                <div class="checkbox-inline">
+                    {!! Form::checkbox('false', 'motpartEksempel' . $bilagsmal->id) !!} Motpart:
+                    <span class="motpartEksempel">{{$bilagsmalsekvens->motpart}}</span>
                 </div>
-            @endforeach
-            <div class="form-group col-sm-12">
-                {!! Form::label('bilagstekst', 'Bilagstekst:') !!}
-                {!! Form::textarea( 'bilagstekst', 'Kontantrabatt ved betaling før 60 dager.', ['id' => 'bilagstekst' . $bilagsmal->id, 'class' => 'form-control localstorage', 'style' => 'height: 75px;']) !!}
+                @foreach($bilagsmalsekvens->variabler as $variabel)
+                    <div class="checkbox-inline">
+                        {!! Form::checkbox('false', $variabel->tegn_i_formel . 'Eksempel' . $bilagsmal->id) !!}
+                        {{$variabel->tegn_i_formel}}:
+                        <span class="{{$variabel->tegn_i_formel}}NavnEksempel">{{$variabel->navn}}:</span>
+                        <span class="{{$variabel->tegn_i_formel}}Eksempel">{{($variabel->verdi_min +$variabel->verdi_min)/2}}</span>
+                    </div>
+                @endforeach
             </div>
         </div>
+        <hr>
+        <div class="row">
+            <div class="col-md-12">
+
+                <div class="form-group">
+                    <p class="lead">Bilagstekst:</p>
+
+                    {!! Form::textarea( 'bilagstekst', 'Kontantrabatt ved betaling før 60 dager.', ['id' => 'bilagstekst' . $bilagsmal->id, 'class' => 'form-control localstorage', 'style' => 'height: 75px;']) !!}
+                </div>
+            </div>
+        </div>
+
         {!! Form::close() !!}
     </div>
     <div class="postering list-group">
@@ -69,11 +77,18 @@
                     {{-- TODO Slett postering i DB --}}
                 </div>
             </div>
-
             {!! Form::close() !!}
         @endforeach
-        <a class="ny-postering row list-group-item list-group-item-info text-center">
-            <span class="fa fa-plus"></span> Legg til postering
-        </a>
+            <div class="list-group-item list-group-item-info ">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="btn-group pull-right">
+                            <a class="btn btn-default" data-toggle="tooltip" data-placement="top" data-container="body" title="Legg til postering">
+                                <span class="fa fa-plus"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
     </div>
 </div>
