c6ce898aefea6a5b79ec9574a79b44d26ece4e02
Jørgen Tellnes <121605@student.hit.no>
2015-04-28 11:26:46 +0200

    Endre lenker / viste datafelter, legg til slett- og rediger-knapper

--- resources/views/besvarelser/testing/vis.blade.php
+++ resources/views/besvarelser/testing/vis.blade.php
@@ -2,17 +2,25 @@
 @section('content')
 
     <div class="container">
-        <h1>Besvarelse på '{{ $besvarelse->oppgavesett->beskrivelse }}'</h1>
+        <h1>Besvarelse på {{ $besvarelse->oppgavesett->beskrivelse }}</h1>
+        <br/>
 
         <p>Av: {{ $besvarelse->skaper->fulltnavn() }}</p>
 
-        <p>Elevens kommentar til besvarelsen: <i>{{ $besvarelse->kommentar }}</i></p>
-        <ul>
-            @foreach($besvarelse->oppgavesvar as $oppgavesvar)
-                <li>Kommentar til oppgavesvar
-                    <b>{{ $oppgavesvar->moduloppgavesvar_id }}</b> : {{ $oppgavesvar->kommentar }}</li>
-            @endforeach
-        </ul>
-        {!! link_to_route('besvarelser.opplist', 'Alle besvarelser') !!}
+        <p>Opprettet: {{ $besvarelse->tidOpprettet() }}</p>
+
+        <p>Leveringsfrist: {{ $besvarelse->oppgavesett->tidLukket() }}</p>
+
+        @if($besvarelse->kommentar != '')
+            <p>Kommentar til besvarelsen: <i>{{ $besvarelse->kommentar }}</i></p>
+        @endif
+
+        <br/>
+        {!! link_to_route('besvarelser.rediger', 'Rediger besvarelse', $besvarelse, ['class' => 'btn btn-default']) !!}
+        {!! Form::submit('Levér besvarelse', ['class' => 'btn btn-warning']) !!}
+        {!! link_to_route('besvarelser.slett', 'Slett besvarelse', $besvarelse, ['class' => 'btn btn-danger']) !!}
+        <br/>
+        <br/>
+        {!! link_to_route('oppgavesett.opplist', 'Oppgavesett/besvarelser') !!}
     </div>
 @endsection
