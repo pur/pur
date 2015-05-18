74c4650a5dccb19fd740eebc0f931e38711c9ead
Benjamin Dehli <superelg@gmail.com>
2015-05-18 20:58:35 +0200

    Oppdater logginn-side

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,41 @@
 @extends('pur')
 
 @section('content')
-    <div class="container">
-        <h1>Logg inn</h1>
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">Logg inn med testbruker</div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-6 text-center">
-                                <p>Logg inn som Lærer Lærd</p>
-                                {!! Form::open(['url' => '/auth/login', 'method' => 'post']) !!}
-                                {!! Form::input('text', 'email', 'laerer@laerd.no') !!}
+    <div class="container innlogging">
 
-                                {!! Form::input('password','password', '') !!}
-                                {!! Form::submit('Logg inn', ['class' => 'btn btn-default']) !!}
-                                {!! Form::close()  !!}
-                            </div>
-                            <div class="col-lg-6 text-center">
-                                <p>Logg inn som Sture Student</p>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <section>
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">Logg inn</div>
+                        <div class="panel-body">
+                            <div class="row">
                                 {!! Form::open(['url' => '/auth/login', 'method' => 'post']) !!}
-                                {!! Form::input('text', 'email', 'sture@student.no') !!}
-                                {!! Form::input('password','password', '') !!}
-                                {!! Form::submit('Logg inn', ['class' => 'btn btn-default']) !!}
-                                {!! Form::close()  !!}
+
+                                <div class="form-group col-sm-6">
+                                    <div class="input-group">
+                                        <div class="input-group-addon">Brukernavn:</div>
+                                        {!! Form::input('text', 'email', null, ['class' => 'form-control']) !!}
+                                    </div>
+                                </div>
+                                <div class="form-group col-sm-6">
+                                    <div class="input-group">
+                                        <div class="input-group-addon">Passord:</div>
+                                        {!! Form::input('password','password', null, ['class' => 'form-control']) !!}
+                                    </div>
+                                </div>
+                                <div class="form-group col-sm-6 pull-right">
+                                    {!! Form::submit('Logg inn', ['class' => 'btn btn-default pull-right']) !!}
+                                </div>
                             </div>
+                            {!! Form::close()  !!}
                         </div>
                     </div>
+                    </section>
                 </div>
             </div>
-        </div>
+
     </div>
+
+
 @endsection
