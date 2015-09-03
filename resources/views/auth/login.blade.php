2142b9fdb10714b56b211430fb6e6c766b085b44
Benjamin Dehli <superelg@gmail.com>
2015-09-03 23:15:21 +0200

    Legg til luft over påloggingsvindu

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,58 +1,62 @@
 @extends('pur')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Logg inn</div>
-                    <div class="panel-body">
-                        @if (count($errors) > 0)
-                            <div class="alert alert-danger">
-                                <strong>Huff da!</strong> Pur var ikke helt fornøyd med det du la inn:<br><br>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-
-                        <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
-                            <input type="hidden" name="_token" value="{{ csrf_token() }}">
-
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">E-post:</label>
-                                <div class="col-md-6">
-                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+    <div class="container innlogging">
+        <div class="space-50">
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">Logg inn</div>
+                        <div class="panel-body">
+                            @if (count($errors) > 0)
+                                <div class="alert alert-danger">
+                                    <strong>Huff da!</strong> Pur var ikke helt fornøyd med det du la inn:<br><br>
+                                    <ul>
+                                        @foreach ($errors->all() as $error)
+                                            <li>{{ $error }}</li>
+                                        @endforeach
+                                    </ul>
                                 </div>
-                            </div>
+                            @endif
 
-                            <div class="form-group">
-                                <label class="col-md-4 control-label">Passord:</label>
-                                <div class="col-md-6">
-                                    <input type="password" class="form-control" name="password">
-                                </div>
-                            </div>
+                            <form class="form-horizontal" role="form" method="POST" action="{{ url('/auth/login') }}">
+                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Husk meg
-                                        </label>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label">E-post:</label>
+
+                                    <div class="col-md-6">
+                                        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
                                     </div>
                                 </div>
-                            </div>
 
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">Logg inn</button>
+                                <div class="form-group">
+                                    <label class="col-md-4 control-label">Passord:</label>
 
-                                    <a class="btn btn-link" href="{{ url('/password/email') }}">Glemt passordet?</a>
+                                    <div class="col-md-6">
+                                        <input type="password" class="form-control" name="password">
+                                    </div>
                                 </div>
-                            </div>
-                        </form>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <div class="checkbox">
+                                            <label>
+                                                <input type="checkbox" name="remember"> Husk meg
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-6 col-md-offset-4">
+                                        <button type="submit" class="btn btn-primary">Logg inn</button>
+
+                                        <a class="btn btn-default" href="{{ url('/password/email') }}">Glemt passordet?</a>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
