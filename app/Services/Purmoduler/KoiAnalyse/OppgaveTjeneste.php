5da5717110b2cd320c0abcc140980c8aeae9d7f1
Jørgen Tellnes <jorgen@tellnes.nu>
2016-06-06 21:52:29 +0200

    Generer alle Instans-variabler fra Datasett i  OppgaveTjeneste

--- app/Services/Purmoduler/KoiAnalyse/OppgaveTjeneste.php
+++ app/Services/Purmoduler/KoiAnalyse/OppgaveTjeneste.php
@@ -18,13 +18,21 @@ class OppgaveTjeneste {
         $instans->bruker()->associate($bruker);
         //$instans->datasett()->associate($datasett); // Nødvendig å kjenne datasettet?
 
-        // TODO: Vurder å beregne instansvariablene i Datasett-klassen
-        $instans->a =  mt_rand($datasett->a_min*100, $datasett->a_maks*100) / 100;
-        // TODO: Beregn resten av instansvariablene
-
+        $instans->a = $this->genererVariabel($datasett->a_min, $datasett->a_maks, $datasett->a_intervall);
+        $instans->b = $this->genererVariabel($datasett->b_min, $datasett->b_maks, $datasett->b_intervall);
+        $instans->d = $this->genererVariabel($datasett->d_min, $datasett->d_maks, $datasett->d_intervall);
+        $instans->m = $this->genererVariabel($datasett->m_min, $datasett->m_maks, $datasett->m_intervall);
+        $instans->n = $this->genererVariabel($datasett->n_min, $datasett->n_maks, $datasett->n_intervall);
+        $instans->q = $this->genererVariabel($datasett->q_min, $datasett->q_maks, $datasett->q_intervall);
+        $instans->kapasitet = $datasett->kapasitet;
 
         $instans->save();
 
         return $instans;
+    }
+
+    public function genererVariabel($min, $max, $intervall)
+    {
+        return $intervall * mt_rand(1, abs(($max - $min) / $intervall)) + $min;
     }
 }
